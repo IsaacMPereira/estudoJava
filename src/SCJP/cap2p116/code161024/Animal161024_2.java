84154a73aa1ede74f989ab139b78cae1718b2ef8
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-10-16 22:01:30 -0300

    Commit com exemplo quebrado

---
+++ src/SCJP/cap2p116/code161024/Animal161024_2.java
@@ -0,0 +1,12 @@
+package SCJP.cap2p116.code161024;
+
+class Animal161024_2 { }
+class Dog implements Pet{
+    @Override
+    public void beFriendly() {
+        
+    }
+}
+interface Pet {
+    void beFriendly();
+}
